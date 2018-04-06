79c06ebe48741a2a7b26309acb9e28fcf73a7162
Mortada AlKhars <mortadaajk@gmail.com>
2018-04-06 20:04:55 +0300

    initial implementation of scheduler

---
+++ lib/aljawad_scheduler.ex
@@ -0,0 +1,18 @@
+defmodule AljawadScheduler do
+  @moduledoc """
+  Documentation for AljawadScheduler.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AljawadScheduler.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
